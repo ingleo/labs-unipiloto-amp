b05fade39f341748aa8f2edd1076410ca8f3194c
Leonardo González <clgrhc@gmail.com>
2016-10-07 21:26:32 -0500

    footer abajo
    se baja el footer a la parte baja de la página

--- Modules/module-II/js/first.js
+++ Modules/module-II/js/first.js
@@ -1,3 +1,11 @@
 function cambiarColor(){
-	document.getElementById('parrafo').style.fontSize='35px';
+
+	var tamano = document.getElementById('parrafo').style.fontSize;
+	if(tamano == '35px'){
+		document.getElementById('parrafo').style.fontSize='25px';	
+		document.getElementById('parrafo').style.color='black';
+	}else{
+		document.getElementById('parrafo').style.fontSize='35px';
+		document.getElementById('parrafo').style.color='green';
+	}
 }
