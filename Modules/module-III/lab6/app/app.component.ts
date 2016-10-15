db44a7cbf7e09a914ff2b894d6828899910e36c7
Leonardo González <clgrhc@gmail.com>
2016-10-15 13:01:55 -0500

    se modifica component
    se modifica component angularjs2

--- Modules/module-III/lab6/app/app.component.ts
+++ Modules/module-III/lab6/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular App</h1>'
+  template: '<h1>{{hello}}</h1>'
 })
-export class AppComponent { }
+export class AppComponent {
+	hello: string = "hola mundo complicado desde angularJS2";
+ }
