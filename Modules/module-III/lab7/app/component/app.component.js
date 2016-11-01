f424ad856e9fe051ecbfb2c84569370127c3783d
Leonardo González <clgrhc@gmail.com>
2016-11-01 02:45:00 -0500

    lab7
    lab7

--- Modules/module-III/lab7/app/component/app.component.js
+++ Modules/module-III/lab7/app/component/app.component.js
@@ -37,11 +37,11 @@ var AppComponent = (function () {
         core_1.Component({
             selector: 'my-app',
             /*template: '<h1>{{product.id}}</h1><br />' +
-                      '<h2>{{product.name}}</h2><br />' +
-                      '<h2>{{product.description}}</h2><br />' +
-                      '<h2>{{product.type}}</h2><br />' +
-                      '<h2>{{product.price}}</h2><br />' +
-                      '<h2>{{product.quantity}}</h2><br />' +
+                          '<h2>{{product.name}}</h2><br />' +
+                          '<h2>{{product.description}}</h2><br />' +
+                          '<h2>{{product.type}}</h2><br />' +
+                          '<h2>{{product.price}}</h2><br />' +
+                          '<h2>{{product.quantity}}</h2><br />' +
                   '<br />' +
                   '<h2>{{user.name}}</h2><br />' +
                   '<h2>{{user.id}}</h2><br />' +
