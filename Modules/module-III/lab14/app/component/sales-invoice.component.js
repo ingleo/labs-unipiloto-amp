0931f47df8854d92d6846113ae00240baf8b1972
Leonardo González <clgrhc@gmail.com>
2016-11-06 12:22:02 -0500

    cambio lab
    cambio lab

--- Modules/module-III/lab14/app/component/sales-invoice.component.js
+++ Modules/module-III/lab14/app/component/sales-invoice.component.js
@@ -31,9 +31,10 @@ var SalesInvoiceComponent = (function () {
             templateUrl: 'app/templates/sales-invoice.html',
             providers: [sales_invoice_service_1.SalesInvoiceService]
         }), 
-        __metadata('design:paramtypes', [sales_invoice_service_1.SalesInvoiceService, router_1.ActivatedRoute])
+        __metadata('design:paramtypes', [sales_invoice_service_1.SalesInvoiceService, (typeof (_a = typeof router_1.ActivatedRoute !== 'undefined' && router_1.ActivatedRoute) === 'function' && _a) || Object])
     ], SalesInvoiceComponent);
     return SalesInvoiceComponent;
+    var _a;
 }());
 exports.SalesInvoiceComponent = SalesInvoiceComponent;
 //# sourceMappingURL=sales-invoice.component.js.map
