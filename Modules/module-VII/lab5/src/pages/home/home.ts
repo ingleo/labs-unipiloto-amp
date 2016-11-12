0af8a66be2dab9c76a6b27827445c12da4caf705
Leonardo González <clgrhc@gmail.com>
2016-11-12 11:49:10 -0500

    cambios
    se crean primeras pages prueba lab5

--- Modules/module-VII/lab5/src/pages/home/home.ts
+++ Modules/module-VII/lab5/src/pages/home/home.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { ListPage } from '../list/list';
+import { List2Page } from '../list2/list2';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  selected: any = false;
 
   product: Product = {
     id: 1,
@@ -20,6 +23,17 @@ export class HomePage {
 
   }
 
+  loadInfo() {
+    this.selected = true;
+  }
+
+  goList() {
+    this.navCtrl.push(ListPage);
+  }
+
+  goListTwo(){
+    this.navCtrl.push(List2Page);
+  }
 }
 
 export class Product {
--- Modules/module-VII/lab6/src/app/app.module.ts
+++ Modules/module-VII/lab6/src/app/app.module.ts
@@ -2,15 +2,11 @@ import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-import { List2Page } from '../pages/list2/list2';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    ListPage,
-    List2Page
+    HomePage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -18,9 +14,7 @@ import { List2Page } from '../pages/list2/list2';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    ListPage,
-    List2Page
+    HomePage
   ],
   providers: []
 })
